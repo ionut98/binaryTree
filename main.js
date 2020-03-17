951c5979e5affa4ecb13b17e54373b19d3af3bd9
ionutc3 <ionutmihoci@yahoo.com>
2020-03-17 16:57:27 +0200

    more broken than last time

--- main.js
+++ main.js
@@ -1,26 +1,56 @@
 const readNodesFromFile = require('./fileUtils');
 const fileName = process.argv[2];
 
+const createNode = (nodeNumber, data, leftChild, rightChild) => ({
+  data,
+  leftChild,
+  rightChild
+});
+
 const createBinaryTree = nodesFromFileList => {
-  const binaryTree = {};
+  const binaryTree = {
+    root: null,
+    nodes: {}
+  };
+
   nodesFromFileList.forEach(node => {
     const { nodeNumber, parentNodeNumber, childType, data } = node;
 
-    binaryTree[node.nodeNumber] = {
-      nodeNumber,
-      data,
-      leftChild: null,
-      rightChild: null
-    };
+    // create new node in tree
+    binaryTree.nodes[node.nodeNumber] = createNode(data, null, null);
 
-    parentNodeNumber !== 0 ? childType === 's' ? binaryTree[parentNodeNumber][leftChild] = 
+    // take reference of the parent of the actual node
+    let parentNode = binaryTree.nodes[parentNodeNumber];
 
+    parentNodeNumber !== '0'
+      ? childType === 's'
+        ? parentNode
+          ? (parentNode[leftChild] = nodeNumber)
+          : (parentNode = createNode(data, nodeNumber, null))
+        : parentNode
+        ? (parentNode[rightChild] = nodeNumber)
+        : (parentNode = createNode(data, null, nodeNumber))
+      : (binaryTree.root = nodeNumber);
+
+    if (parentNode === '0') {
+      binaryTree.root = nodeNumber;
+    } else {
+      if (childType === 's') {
+        if (!parentNode) {
+          parentNode = createNode(data, nodeNumber, null);
+        } else {
+          parentNode[leftChild] = nodeNumber;
+        }
+      }
+    }
   });
+
+  return binaryTree;
 };
 
 const main = async () => {
   const nodesFromFileList = await readNodesFromFile(fileName);
-  console.log(nodesFromFileList);
+  console.log(createBinaryTree(nodesFromFileList));
 };
 
 main();
